69f3eea597ec453f80edaf9f00a5fe1816b37f8b
Andy <liumin@cgaii.com>
2023-02-21 10:33:19 +0800

    viapp 1.0.0
    更新viapp

---
+++ assets/_snowpack/pkg/react-router.js
@@ -0,0 +1,8 @@
+export {u as useLocation} from "./common/react-router-b73a226e.js";
+import "./common/setPrototypeOf-8347dfab.js";
+import "./common/index-86c632b0.js";
+import "./common/_commonjsHelpers-8c19dec8.js";
+import "./common/extends-f6c17942.js";
+import "./common/index-8ab56611.js";
+import "./common/index-6bd1d6e3.js";
+import "./common/hoist-non-react-statics.cjs-ec82709f.js";
--- assets/index.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector("body > button").addEventListener("click", () => {
-        window.alert("You clicked a button.")
-    })
-});
